e1b8c865add5f926cb25b57dd2a405a9a2d1bd21
kevindwi <kevindwi356@gmail.com>
2025-07-31 13:37:55 +0700

    feat: add leads routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LeadController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,6 +12,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    /* Leads */
+    Route::get('leads', [LeadController::class, 'index'])->name('leads.index');
+    Route::post('leads', [LeadController::class, 'store'])->name('leads.store');
 });
 
 require __DIR__.'/settings.php';
