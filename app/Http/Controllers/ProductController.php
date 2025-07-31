e8e75dfbf3f0a905e5c74fd75398243915c7be32
kevindwi <kevindwi356@gmail.com>
2025-07-31 18:00:21 +0700

    feat: add ProductController index & store

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Product $product)
+    {
+        return Inertia::render("products/index", [
+            'products' => $product->all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'product_name' => 'required|max:255|min:2',
+            'speed' => 'required|min:2',
+            'price' => 'required',
+            'description' => 'nullable|string',
+        ]);
+
+        Product::query()->create($validate);
+
+        return back()->with('message', 'Product added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
