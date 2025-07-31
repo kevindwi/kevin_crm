d448a904bf8b39e791f050f722b0dc78d5b7378e
kevindwi <kevindwi356@gmail.com>
2025-08-01 03:34:20 +0700

    Create 2025_07_31_160358_add_nullable_to_manager_id_on_projects_table.php

---
+++ database/migrations/2025_07_31_160358_add_nullable_to_manager_id_on_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign(['manager_id']);
+
+            $table->unsignedBigInteger('manager_id')->nullable()->change();
+
+            $table->foreign('manager_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign(['manager_id']);
+
+            $table->unsignedBigInteger('manager_id')->nullable(false)->change();
+
+            $table->foreign('manager_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+};
