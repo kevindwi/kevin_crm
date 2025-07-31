7e9e0f6ef5e3e5708896cc9365b8c085a0de7a16
kevindwi <kevindwi356@gmail.com>
2025-07-31 18:02:23 +0700

    feat: add new nav items

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, FolderKanban, LayoutGrid, PackageSearch, UserCheck, Users } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -12,6 +12,26 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: '/dashboard',
         icon: LayoutGrid,
+    },
+    {
+        title: 'Leads',
+        href: '/leads',
+        icon: Users,
+    },
+    {
+        title: 'Products',
+        href: '/products',
+        icon: PackageSearch,
+    },
+    {
+        title: 'Projects',
+        href: '/projects',
+        icon: FolderKanban,
+    },
+    {
+        title: 'Customers',
+        href: '/customers',
+        icon: UserCheck,
     },
 ];
 
