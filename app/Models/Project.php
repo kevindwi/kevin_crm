0f286c477e271b1aaba19652e326163b58582961
kevindwi <kevindwi356@gmail.com>
2025-08-01 16:53:25 +0700

    small change

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
+    use HasFactory;
+
     protected $table = 'projects';
     protected $fillable = [
         'lead_id',
