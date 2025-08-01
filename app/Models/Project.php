5fff7c53c869299d0becef94438d2d9163b314e6
kevindwi <kevindwi356@gmail.com>
2025-08-01 14:49:07 +0700

    Create Project.php

---
+++ app/Models/Project.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Project extends Model
+{
+    protected $table = 'projects';
+    protected $fillable = [
+        'lead_id',
+        'sales_id',
+        'product_id',
+        'status',
+        'manager_id',
+        'approval_date',
+        'notes',
+    ];
+
+    public function lead()
+    {
+        return $this->belongsTo(Lead::class);
+    }
+
+    public function sales(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sales_id');
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
