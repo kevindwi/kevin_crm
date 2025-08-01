c892154c5df069dae07bf452b36d1e64281b1407
kevindwi <kevindwi356@gmail.com>
2025-08-01 14:49:01 +0700

    Create CustomerProduct.php

---
+++ app/Models/CustomerProduct.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerProduct extends Model
+{
+    use HasFactory;
+
+    protected $table = 'customer_products';
+
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'start_date',
+        'is_active',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
